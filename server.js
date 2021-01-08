57963bd91c639740ab5b3def9f1c6a4d2ba5bde7
samuel112 <senaymokie@gmail.com>
2021-01-08 01:20:13 +0000

    l

--- server.js
+++ server.js
@@ -40,9 +40,12 @@ app.post("/name", createName);
 // port
 const port = process.env.PORT || 8000;
 const reactPath = path.resolve(__dirname, "..", "..", "my-app", "build");
-console.log(reactPath);
+//console.log(reactPath);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, () => console.log(`Server is running good on port ${port}`));
