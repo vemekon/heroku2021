89afc0b1824b4afb182613e3f5888248506b7206
samuel112 <senaymokie@gmail.com>
2021-01-08 01:26:20 +0000

    l

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const port = process.env.PORT || 8000;
 //   });
 // }
 
-const publicPath = path.join(__dirname, "..", "build");
+const publicPath = path.join(__dirname, "build");
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
